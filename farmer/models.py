05635b6901a44cfe1ecfc4b2369e1a01144089fb
youdass101 <hamzechalhoub@gmail.com>
2020-11-12 17:26:52 +0200

    udate

--- farmer/models.py
+++ farmer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return f"Username: {self.username}"
+
--- farmer/views.py
+++ farmer/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
-    return render (request, "farmer/index.html")
+    if request.user.is_authenticated:
+        return render (request, "farmer/index.html")
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "farmer/login.html", {
+                "message": "Invalid username or password."
+            })  
+    else:
+        return render(request, "farmer/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
