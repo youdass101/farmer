bc60198484a4d9abed1a82abe5056f356fff1384
youdass101 <hamzechalhoub@gmail.com>
2025-09-09 01:00:35 +0300

    redesign

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]  # or os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
