35cfd7335fc412b3448351cbf7d4be059b1a817f
youdass101 <hamzechalhoub@gmail.com>
2025-09-28 17:42:19 +0300

    preping for production

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -132,5 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  # or os.path.join(BASE_DIR, "static")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # or os.path.join(BASE_DIR, '
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
