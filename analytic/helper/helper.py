820b13bbf3b39ce1a438a7452a3dd1b228aff466
youdass101 <hamzechalhoub@gmail.com>
2025-11-01 23:25:34 +0200

    no clue

--- analytic/helper/helper.py
+++ analytic/helper/helper.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 from django.db.models import Avg, Count
-from farmer.models import Tray
+from farmer.models import Tray, Harvest, Plant
 from datetime import date, timedelta
 from django.utils import timezone
-from farmer.models import Harvest
+
 
 
 def today_plan():
     # Group by Tray.name, calculate average number per day
-    today = date.today()
-    django_weekday = today.weekday() + 2  # Python Monday=0, Django Monday=2
-    trays = Tray.objects.filter(name__active=True, start__week_day=django_weekday)
+    today = timezone.now()
+    #django_weekday = today.weekday() + 2  # Python Monday=0, Django Monday=2
+    # trays = Tray.objects.filter(name__active=True, start__week_day=django_weekday)
+    end_date = timezone.now()
+    start_date = (end_date - timedelta(days=80)).date()  # 1 year
+    print(start_date)
+    trays = Tray.objects.filter(name__active=True, start=start_date)
+
     tray_stats = (
         trays.values('name__name')
         .annotate(
             total_trays=Count('id'),
-            total_weeks=Count('start__week', distinct=True),
-            avg_per_week=Count('id') / Count('start__week', distinct=True)
+            #total_weeks=0,  #Count('start__week', distinct=True),
+            avg_per_week=Count('id')  #Count('id') / Count('start__week', distinct=True)
         )
     )
     context = {
@@ -26,3 +31,4 @@ def today_plan():
     }
     print(context)
     return context
+
